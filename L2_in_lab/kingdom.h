6994f13439f78c6bc2b51646e75e0a6c882c1216
gayanebabayan <g.babayan@yahoo.com>
2019-05-02 00:48:06 -0400

    Update kingdom.h

--- L2_in_lab/kingdom.h
+++ L2_in_lab/kingdom.h
@@ -1,28 +1,13 @@
-// OOP244_B: Workshop -2
-// Date:     23-Jan-2017
-// Author:   Gayane Babayan
-// ID:       107-061-160 
-// email:    gbabayan@myseneca.ca
-
-
-// TODO: header safeguards
 #ifndef SICT_KINGDOM_H_
 #define SICT_KINGDOM_H_
 
-
-   // TODO: westeros namespace
 namespace westeros {
-   // TODO: define the class Kingdom in the westeros namespace
    struct Kingdom {
    char m_name[32];
    int m_population;
    };
 
-   // TODO: add the declaration for the function display(...),
-   //         also in the westeros namespace
-
    void display (struct Kingdom& K);
-
 }
 
 #endif
