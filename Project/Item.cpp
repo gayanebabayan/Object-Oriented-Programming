45c5a19ca200f187cfcbbe97dc05244f17cb6dd1
gayanebabayan <g.babayan@yahoo.com>
2019-05-02 01:36:06 -0400

    Update Item.cpp

--- Project/Item.cpp
+++ Project/Item.cpp
@@ -1,11 +1,3 @@
-// OOP244_B: Final Project - Milestone3
-// File:     Item.cpp
-// Date:     31-March-2017
-// Author:   Gayane Babayan
-// ID:       107-061-160 
-// email:    gbabayan@myseneca.ca
-
-
 #define _CRT_SECURE_NO_WARNINGS  
 // Lib includes
 #include <iostream>
