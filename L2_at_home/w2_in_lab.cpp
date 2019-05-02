5f80bee76cfe29ee3951b1869d8af17d83e54770
gayanebabayan <g.babayan@yahoo.com>
2019-05-02 00:46:16 -0400

    Update w2_in_lab.cpp

--- L2_at_home/w2_in_lab.cpp
+++ L2_at_home/w2_in_lab.cpp
@@ -1,10 +1,3 @@
-// OOP244_B: Workshop -2
-// Date:     23-Jan-2017
-// Author:   Gayane Babayan
-// ID:       107-061-160 
-// email:    gbabayan@myseneca.ca
-
-
 #include <iostream>
 #include "kingdom.h"
 
