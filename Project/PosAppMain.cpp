c42b411047884aebfa7d07d497ff157268243e19
gayanebabayan <g.babayan@yahoo.com>
2019-05-02 01:43:01 -0400

    Update PosAppMain.cpp

--- Project/PosAppMain.cpp
+++ Project/PosAppMain.cpp
@@ -1,10 +1,3 @@
-// OOP244_B: Final Project - Milestone5
-// File:     PosAppMain.cpp
-// Date:     11-April-2017
-// Author:   Gayane Babayan
-// ID:       107-061-160 
-// email:    gbabayan@myseneca.ca
-
 #include "PosApp.h"
 int main() {
    ict::PosApp app("posapp.txt", "bill.txt");
