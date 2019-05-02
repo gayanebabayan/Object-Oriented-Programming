8f5531483695cb779e0d3f7d5c244511f0bfa067
gayanebabayan <g.babayan@yahoo.com>
2019-05-02 00:45:53 -0400

    Update kingdom.h

--- L2_at_home/kingdom.h
+++ L2_at_home/kingdom.h
@@ -1,18 +1,12 @@
 #ifndef SICT_KINGDOM_H_
 #define SICT_KINGDOM_H_
 
-
-   // TODO: westeros namespace
 namespace westeros {
    
-	// TODO: define the class Kingdom in the westeros namespace
    struct Kingdom {
    char m_name[32];
    int m_population;
    };
-
-   // TODO: add the declaration for the function display(...),
-   //         also in the westeros namespace
 
    void display(Kingdom&);
    void display(Kingdom K[], int count);
