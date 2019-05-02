496b40d5f9a2a7af70361fb009fed511a3f63466
gayanebabayan <g.babayan@yahoo.com>
2019-05-02 00:41:19 -0400

    Add files via upload

---
+++ L1_at_home/tools.h
@@ -0,0 +1,19 @@
+/*
+Name:  Gayane Babayan
+ID:    107-061-160
+Email: gbabayan@myseneca.ca
+*/
+
+#ifndef SICT_TOOLS_H_
+#define SICT_TOOLS_H_
+
+namespace sict{
+   // Performs a fool-proof integer entry
+   int getInt(int min, int max);
+   // Fills the samples array with the statistic samples
+   int menu();
+   // Finds the largest sample in the samples array, if it is larger than 70,
+   // therwise it will return 70.
+}
+
+#endif
